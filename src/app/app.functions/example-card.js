144902609e27ba17941177ea91bb855a0cab4ef5
Seth Kaplan <skaplan@hubspot.com>
2023-05-15 15:03:03 -0400

    add hello world

---
+++ src/app/app.functions/example-card.js
@@ -0,0 +1,10 @@
+exports.main = async (context = {}, sendResponse) => {
+  const {text} = context.parameters
+
+  const ret = `This is coming from a serverless function! You entered: ${text}`
+  try {
+    sendResponse(ret);
+  } catch (error) {
+    sendResponse(error);
+  }
+};
