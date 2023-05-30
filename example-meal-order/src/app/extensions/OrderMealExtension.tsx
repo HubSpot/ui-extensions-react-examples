e6e00d8c2065cc561a71e8d3477aa5d41ecaea9a
Danny Shekhtman <dshekhtman@hubspot.com>
2023-05-30 15:10:38 -0400

    Add missing things

--- example-meal-order/src/app/extensions/OrderMealExtension.tsx
+++ example-meal-order/src/app/extensions/OrderMealExtension.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { hubspot } from '@hubspot/ui-extensions';
 import { OrderMealCard } from './components/OrderMealCard';
 
