8e2ce4a4c8549e15c68bce9548aa0413239fdb27
Danny Shekhtman <dshekhtman@hubspot.com>
2023-06-12 15:33:42 -0400

    Add image

--- example-meal-order/src/app/extensions/components/RestaurantRow.tsx
+++ example-meal-order/src/app/extensions/components/RestaurantRow.tsx
@@ -5,6 +5,11 @@ import {
   Text,
   TableRow,
   TableCell,
+  Image,
+  Stack,
+  Tile,
+  Box,
+  Link,
 } from '@hubspot/ui-extensions';
 import { RestaurantRowProps } from '../types';
 import { Rating } from './Rating';
@@ -12,15 +17,20 @@ import { Rating } from './Rating';
 const timeRange = (minutes: number) => `${minutes - 5}-${minutes + 5} min`;
 
 export const RestaurantRow = ({ restaurant, onClick }: RestaurantRowProps) => {
-  const { name, category, deliveryCost, deliveryInMinutes, rating } =
+  const { name, category, deliveryCost, deliveryInMinutes, rating, image } =
     restaurant;
 
   return (
     <TableRow>
       <TableCell>
-        <Heading>{name}</Heading>
-        <Text variant="microcopy">{category}</Text>
-        <Rating value={rating} />
+        <Stack distance="xs">
+          <Image src={image} width={80} />
+          <Stack direction="column" distance="flush">
+            <Heading>{name}</Heading>
+            <Text variant="microcopy">{category}</Text>
+            <Rating value={rating} />
+          </Stack>
+        </Stack>
       </TableCell>
       <TableCell>
         <Text>{timeRange(deliveryInMinutes)}</Text>
